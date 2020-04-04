41521bc258ea1daef96b2ea822c309d7f77cd76c
carreterosanti <unicen123>
2020-04-03 21:07:27 -0300

    Updates

---
+++ precache-manifest.f783dff65e10876c7f5c047ce5e1528f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0d648af4ae59be41709e6d5a6983fc",
+    "url": "/gesture-calculator/index.html"
+  },
+  {
+    "revision": "d071be71207b24ff1398",
+    "url": "/gesture-calculator/static/css/main.c759a67c.chunk.css"
+  },
+  {
+    "revision": "9325a096d19352637ca7",
+    "url": "/gesture-calculator/static/js/2.420af07c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gesture-calculator/static/js/2.420af07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d071be71207b24ff1398",
+    "url": "/gesture-calculator/static/js/main.12bb1bcc.chunk.js"
+  },
+  {
+    "revision": "99a53c3488f0405cd716",
+    "url": "/gesture-calculator/static/js/runtime-main.1adaea52.js"
+  }
+]);
